48f686b3f5dbc50b52f98386d2bc98c1840bd9cc
Akshita-web <you@akshita29320@gmail.com>
2020-07-28 11:32:03 +0530

    added effects and drawn some portion like its face

--- face.js
+++ face.js
@@ -3,4 +3,35 @@ var can=document.getElementById("canvas");
 var ct=can.getContext("2d");
 //calculating the radius of clock using height of canvas
 var radius=can.height/2;
-//making clock adjustable for all
+//making clock adjustable for all walls
+ct.translate(radius,radius);
+//reducing the radius of clock by 90% so that it can perfectly fit into canvas object
+radius=radius*.90;
+//creating a funcion to draw a clock
+function drawclock()
+{
+  ct.arc(0,0,radius,0,2*Math.PI);
+   ct.fillStyle="red";
+   ct.fill();
+   drawFace(ct,radius);
+   
+}
+drawclock();
+function drawFace(ct,radius)
+{
+    //drawing the inner structure of clock using beginpath
+    ct.beginPath();
+    ct.arc(0,0,radius,0,2*Math.PI);
+    ct.fillStyle="grey";
+    ct.fill();
+    grad = ct.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+/*creating 3D effect in clock by adding 3 color stops for outer,middle,inner edge
+of arc*/
+grad.addColorStop(0, '#333');
+grad.addColorStop(0.5, 'white');
+grad.addColorStop(1, '#333');
+ct.strokeStyle=grad;
+ct.lineWidth = radius*0.1;
+  ct.stroke();
+  
+}
