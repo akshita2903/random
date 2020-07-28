40cc514e1da16413613c21858de32d595151a5d5
Akshita-web <you@akshita29320@gmail.com>
2020-07-28 10:55:58 +0530

    first commit

---
+++ face.js
@@ -0,0 +1,6 @@
+var can=document.getElementById("canvas");
+//creating a 2d drawing object ct for can object
+var ct=can.getContext("2d");
+//calculating the radius of clock using height of canvas
+var radius=can.height/2;
+//making clock adjustable for all
